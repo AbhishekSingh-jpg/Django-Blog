7f70143ce1c0317d4cd3f55dfcf4246c731901f7
Abhishek Singh <abhishek.rajiv.singh@gmail.com>
2021-01-14 00:55:15 +0530

    First Commit

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model):
+    post_related=models.ForeignKey(Post,on_delete=models.CASCADE)
+    user=models.CharField(max_length=100)
+    date_published=models.DateTimeField(default=timezone.now)
+    body=models.TextField()
+
+    def __str__(self):
+        return f'{self.post_related.title}-{self.user}'
